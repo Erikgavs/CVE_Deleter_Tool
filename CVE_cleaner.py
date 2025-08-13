935724aae3e11cd2c28226cea754dc42a6b89383
Erik <erikgavs@gmail.com>
2025-08-13 23:35:50 +0200

    script acabado

--- CVE_cleaner.py
+++ CVE_cleaner.py
@@ -8,7 +8,7 @@ texto = input("\nProporciona algúna palabra que contenga el archivo que quieras
 
 print(f"\nLa ruta seleccionada es: {Ruta} \n la palabra de los archivos es: {texto}")
 
-confirm = input("\nsi la ruta y la palabra clave son correctas escribe y, si es incorrecto n: \n")
+confirm = input("\nsi la ruta y la palabra clave son correctas escribe y\, si es incorrecto n\: \n\n")
 
 # EL USUARIO CONFIRMA QUE LAS RUTAS SON LAS CORRECTAS
 if confirm.lower() == "y":
@@ -32,15 +32,15 @@ archivos = os.listdir(Ruta)
 
 for archivo in archivos:
     if texto in archivo:
-        print(f"\n[*] Archivo encontrado: {archivos}")
+        print("\n[*] Archivo o carpeta encontrada")
 
         # BORRADO DEL ARCHIVO
         ruta_completa = os.path.join(Ruta, archivo)
         if os.path.isfile(ruta_completa):
             os.remove(ruta_completa)
-            print(f"\nArchivos eliminados {archivos}")
+            print(f"\nArchivo eliminado correctamente: {archivo}\n")
         elif os.path.isdir(ruta_completa):
             shutil.rmtree(ruta_completa) # BORRA CARPETA Y Su contenido
-            print(f"\nCarpetas eliminadas {archivos}")
+            print(f"\nCarpeta eliminada correctamente: {archivo} \n")
         else:
             print("\n[-] Error al borrar los directorios o archivos")
